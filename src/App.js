370464af7db0fb743e7172520f3dab38f3b7834e
Laksmin-Monteiro-Bittencourt <lakshmi96@yahoo.com.br>
2022-06-28 16:35:14 -0300

    só comitando aqui rapidao

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import React from "react";
-class  App extends React.Component {
+import Styled from "styled-components";
 
+const Body = Styled.div `
+  margin: 0px;
+`
 
+const Headerzin = Styled.header `
+  background-color: grey;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
+`
+const Titulo = Styled.h1 `
+  margin-left: 10px;
+`
 
-  render(){
-  return (
-    <div >
-     <h1>labeninja</h1>
+const CardButton = Styled.div `
+  background-color: grey;
+  gap: 20px;
+  
+`
+export default class  App extends React.Component {
 
-    </div>
-  );
+  render() {
+
+    return (
+
+      <Body>
+
+        <Headerzin>
+          <Titulo>LabeNinjas</Titulo>
+          <CardButton>
+          <button>Home</button>
+          <button>Carrinho</button>
+          </CardButton>
+        </Headerzin>
+
+      </Body>
+
+    );
+
+  }
+  
 }
-}
 
-export default App;
+
