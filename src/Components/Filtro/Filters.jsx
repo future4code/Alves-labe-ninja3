e6cb907be8ee4cb29ff53507dc25973a1d6a6a84
Wilson Santos de OLiveira <w.s.oliveira12@hotmail.com>
2022-06-29 16:40:34 -0300

    filtros finalizados

--- src/Components/Filtro/Filters.jsx
+++ src/Components/Filtro/Filters.jsx
@@ -1,40 +1,37 @@
 import React from "react";
-import axios from "axios";
 
-export class Filters extends React.Component{
-state={
+export class Filters extends React.Component {
 
-}
+    render() {
+        return <div>
+            <label>
+                <input
+                    type="number"
+                    placeholder="valor minimo"
+                    value={this.props.valorMinimo}
+                    onChange={this.props.pegaValorMinimo}
+                />
+            </label>
+            <label>
+                <input type="number"
+                    placeholder="valor máximo"
+                    value={this.props.valorMaximo}
+                    onChange={this.props.pegaValorMaximo} />
+            </label>
+            <label>
+                <input
+                    type="text"
+                    placeholder="nome ou descrição"
+                    value={this.props.buscaNome}
+                    onChange={this.props.pegaNome} />
+            </label>
+            <select onChange={this.props.pegaOrdem}>
+                <option value={"priceCR"}>Preço Crescente</option>
+                <option value={"priceDR"}>Preço Decrescente</option>
+                <option valeu={"title"}>Nome</option>
+                <option value={"dueDate"}>Prazo</option>
+            </select>
+        </div>
 
-render(){
-return<div>
-    <label>
-        <input 
-        type="number" 
-        placeholder="valor minimo" 
-        value={this.props.valorMinimo}
-        onChange={this.props.pegaValorMinimo}
-        />
-    </label>
-    <label>
-        <input type="number" 
-        placeholder="valor máximo" 
-        value={this.props.valorMaximo}
-        onChange={this.props.pegaValorMaximo}/>
-    </label>
-    <label>
-        <input 
-        type= "text"
-        placeholder="busca por nome" 
-        value={this.props.buscaNome}
-        onChange={this.props.pegaNome}/>
-    </label>
-    <select>
-        <option valeu={"title"}>Nome</option>
-        <option value={"price"}>Preço</option>
-        <option value={"dueDate"}>Data</option>
-    </select>
-</div>
-
-}
+    }
 }
