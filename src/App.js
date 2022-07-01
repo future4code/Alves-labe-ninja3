e4ca55109203155e7239c837b3349aebe67119b6
Gabriel Dos Santos Silva <gabrieldos55570000@gmail.com>
2022-07-01 12:17:45 -0300

    carrinho com suas funcionalidades para testes junto a api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import CadastroLaks from './Components/Cadastro/CadastroLaks'
 import HomeLaks from './Components/Home/HomeLaks'
 import { FiltersComponets } from './Components/Filtro/FiltersComponets'
+import { Carrinho } from './Components/Carrinho/Carrinho'
 
 //Estilização
 const Div = styled.div`
@@ -27,7 +28,10 @@ const CardButton = styled.div`
 
 export default class App extends React.Component {
   state = {
-    telaAtual: 'home'
+    telaAtual: 'home',
+    carrinho: [],
+    valorTotalCarrinho:'',
+
   }
 
   trocarTela = () => {
@@ -36,8 +40,10 @@ export default class App extends React.Component {
         return <HomeLaks irParaCadastroLaks={this.irParaCadastroLaks} />
       case 'cadastro':
         return <CadastroLaks irParaHome={this.irParaHome} />
+        case 'carrinho': 
+        return <Carrinho calculaValorTotal={this.calculaValorTotal} finalizarCompra={this.finalizarCompra}  excluirItemCarrinho ={this.excluirItemCarrinho}/>
 
-      // mais 2 case com a pagina de contratação e botao carrinho aqui antes do default
+      // mais 1 case com a pagina de contratação aqui antes do default
 
       default:
         return <div>Ops! Página não encontrada.</div>
@@ -54,6 +60,32 @@ export default class App extends React.Component {
     this.setState({ telaAtual: 'cadastro' })
   }
 
+ //Funções do carrinho:
+
+  irParaCarrinho = ()=>{
+    this.setState({telaAtual: 'carrinho'})
+  }
+
+  calculaValorTotal = ()=>{
+    this.setState({
+      valorTotalCarrinho: this.state.valorTotalCarrinho + (104 + (10 + 103) + (102 + 11))
+    })
+  }
+
+  finalizarCompra =()=>{
+    this.setState({carrinho:"", valorTotalCarrinho:""})
+  }
+
+  excluirItemCarrinho = (produtoParaRemover)=>{
+    const novoCarrinho = this.state.carrinho.filter(item=>{
+      if(item.id !== produtoParaRemover.id){
+        return item
+      }
+      // console.log("novo carrinho",novoCarrinho)
+    })
+    console.log("novo carrinho",novoCarrinho)
+}
+
   render() {
     return (
       <Div>
@@ -67,7 +99,7 @@ export default class App extends React.Component {
           <Titulo>LabeNinjas</Titulo>
           <CardButton>
             <button onClick={this.irParaHome}>Home</button>
-            <button>Carrinho</button>
+            <button onClick={this.irParaCarrinho}>Carrinho</button>
   
           </CardButton>
         </Headerzin>
