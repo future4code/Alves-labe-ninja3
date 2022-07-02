107eeecb45f5463e98a358a07cb1cd3bcd5c9e3d
Ayrton Medrano <mrayrtonmedrano@gmail.com>
2022-07-02 15:51:53 -0300

    mudancas

--- src/App.js
+++ src/App.js
@@ -5,35 +5,33 @@ import CadastroLaks from './Components/Cadastro/CadastroLaks'
 import HomeLaks from './Components/Home/HomeLaks'
 import { FiltersComponets } from './Components/Filtro/FiltersComponets'
 import { Carrinho } from './Components/Carrinho/Carrinho'
-
+import VerDetalhes from './Components/CardDetalhes/VerDetalhes'
 //Estilização
 const Div = styled.div`
   margin: 0px;
 `
 
 const Headerzin = styled.header`
-
   display: flex;
   align-items: center;
   justify-content: space-between;
- border-bottom:dotted;
+  border-bottom: dotted;
 `
 const Titulo = styled.h1`
   margin-left: 10px;
-
 `
 
 const CardButton = styled.div`
   gap: 20px;
-  margin-right:50px;
+  margin-right: 50px;
 `
 
 export default class App extends React.Component {
   state = {
     telaAtual: 'home',
     carrinho: [],
-    valorTotalCarrinho:'',
-
+    valorTotalCarrinho: '',
+    botaoDetalhes: ''
   }
 
   trocarTela = () => {
@@ -42,16 +40,37 @@ export default class App extends React.Component {
         return <HomeLaks irParaCadastroLaks={this.irParaCadastroLaks} />
       case 'cadastro':
         return <CadastroLaks irParaHome={this.irParaHome} />
-        case 'carrinho': 
-        return <Carrinho calculaValorTotal={this.calculaValorTotal} finalizarCompra={this.finalizarCompra}  excluirItemCarrinho ={this.excluirItemCarrinho}/>
-
+      case 'carrinho':
+        return (
+          <Carrinho
+            calculaValorTotal={this.calculaValorTotal}
+            finalizarCompra={this.finalizarCompra}
+            excluirItemCarrinho={this.excluirItemCarrinho}
+          />
+        )
+      case 'detalhes':
+        return (
+          <VerDetalhes
+            id={this.state.botaoDetalhes}
+            irParaPaginaTela={this.irParaPaginaTela}
+          />
+        )
       // mais 1 case com a pagina de contratação aqui antes do default
+      // Adicionar como props this.irParaPaginaDetalhes no case da Melissa
 
       default:
         return <div>Ops! Página não encontrada.</div>
     }
     console.log(this.state.telaAtual)
-    
+  }
+
+  irParaPaginaDetalhes = id => {
+    this.setState({ telaAtual: 'detalhes', botaoDetalhes: id })
+  }
+
+  //Dentro da tela atual abaixo vai o nome da tela da melissa
+  irParaPaginaTela = () => {
+    this.setState({ telaAtual: '', botaoDetalhes: '' })
   }
 
   irParaHome = () => {
@@ -62,38 +81,36 @@ export default class App extends React.Component {
     this.setState({ telaAtual: 'cadastro' })
   }
 
- //Funções do carrinho:
+  //Funções do carrinho:
 
-  irParaCarrinho = ()=>{
-    this.setState({telaAtual: 'carrinho'})
+  irParaCarrinho = () => {
+    this.setState({ telaAtual: 'carrinho' })
   }
 
-  calculaValorTotal = ()=>{
+  calculaValorTotal = () => {
     this.setState({
-      valorTotalCarrinho: this.state.valorTotalCarrinho + (104 + (10 + 103) + (102 + 11))
+      valorTotalCarrinho:
+        this.state.valorTotalCarrinho + (104 + (10 + 103) + (102 + 11))
     })
   }
 
-  finalizarCompra =()=>{
-    this.setState({carrinho:"", valorTotalCarrinho:""})
+  finalizarCompra = () => {
+    this.setState({ carrinho: '', valorTotalCarrinho: '' })
   }
 
-  excluirItemCarrinho = (produtoParaRemover)=>{
-    const novoCarrinho = this.state.carrinho.filter(item=>{
-      if(item.id !== produtoParaRemover.id){
+  excluirItemCarrinho = produtoParaRemover => {
+    const novoCarrinho = this.state.carrinho.filter(item => {
+      if (item.id !== produtoParaRemover.id) {
         return item
       }
       // console.log("novo carrinho",novoCarrinho)
     })
-    console.log("novo carrinho",novoCarrinho)
-}
+    console.log('novo carrinho', novoCarrinho)
+  }
 
   render() {
     return (
       <Div>
-
-       
-        
         <Headerzin>
           <Titulo>LabeNinjas</Titulo>
           <CardButton>
@@ -101,11 +118,10 @@ export default class App extends React.Component {
             <button onClick={this.irParaCarrinho}>Carrinho</button>
           </CardButton>
         </Headerzin>
- <FiltersComponets/>
-       <TelaExibir/>
+        <FiltersComponets />
+        <TelaExibir />
 
         {this.trocarTela()}
-        
       </Div>
     )
   }
